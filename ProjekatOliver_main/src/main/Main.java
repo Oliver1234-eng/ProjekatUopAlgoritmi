3e6c4b2c019a4c83e49ea885236251c9acfe8cde
Oliver1234-eng <oliver.kojic22@gmail.com>
2021-06-25 15:05:04 +0200

    Poslednje provere i napomene

--- ProjekatOliver_main/src/main/Main.java
+++ ProjekatOliver_main/src/main/Main.java
@@ -36,6 +36,25 @@ public class Main {
 		taksiSluzba.ucitajMusterije(MUSTERIJE_FAJL);
 		taksiSluzba.ucitajVoznje(VOZNJE_FAJL);
 		
+		/* NAPOMENE
+		 * Kao i za kt2, i sada sam ovde u main podesio sve prozore na setVisible(true), pa æe prilikom pokretanja
+		 * sve da iskoce jedna preko druge, a moguce je jedan prozor ostaviti na true, a sve ostalo promeniti na false
+		 * da samo jedan iskoci. Sve funkcionalnosti sam implementirao vezano za UOP deo i uradio navigaciju. Nova 
+		 * dodavanja ili izmene nece biti odmah vidljive u tabelama, nego treba izaci pa opet uci da bi se
+		 * osvezilo (a u txt fajlovima se odmah uocava svaka promena).
+		 * 
+		 * Dispecer dodeljuje voznju vozacu tako sto ga odabere iz liste na osnovu id-a (username). Jedino da napomenem
+		 * unapred da, kada vozac prihvata/odbija voznju i kada ga zavrsava, onda mu se otvara forma i na toj formi se
+		 * nekako ime vozaca vrati na default-nu vrednost (null), pa ga treba u toj listi ponovo odabrati 
+		 * (svakako ostaje da je njemu dodeljena, nego u formi se ne znam iz kog razloga vrati na null vrednost).
+		 * 
+		 * Sto se ostatka tice, nemam nikakvih napomena. CRUD se moze pokrenuti kao prozor i ovde u main-u, ali i kod
+		 * ulogovanog dispecera ima JMenu sa CRUD-om za vozace, voznje i dispecere i moze se i tako otvoriti, ali u oba
+		 * slucaja radi posao.
+		 */
+		
+		//LOGIN PROZORI ZA SVAKU ULOGU KORISNIKA
+		
 		LoginProzorDispecer loginProzorDispecer = new LoginProzorDispecer(taksiSluzba);
 		loginProzorDispecer.setVisible(true);
 		
@@ -44,14 +63,16 @@ public class Main {
 		
 		LoginProzorMusterija loginProzorMusterija = new LoginProzorMusterija(taksiSluzba);
 		loginProzorMusterija.setVisible(true);
+		
+		//CRUD za dispecere, vozace i voznje
 	
-//		DispeceriProzorDispecer dispeceriProzorDispecer = new DispeceriProzorDispecer(taksiSluzba);
-//		dispeceriProzorDispecer.setVisible(false);
-//		
-//		VozaciProzorDispecer vozaciProzorDispecer = new VozaciProzorDispecer(taksiSluzba);
-//		vozaciProzorDispecer.setVisible(false);
-//		
-//		VoznjeProzorDispecer voznjeProzorDispecer = new VoznjeProzorDispecer(taksiSluzba);
-//		voznjeProzorDispecer.setVisible(false);
+		DispeceriProzorDispecer dispeceriProzorDispecer = new DispeceriProzorDispecer(taksiSluzba);
+		dispeceriProzorDispecer.setVisible(true);
+		
+		VozaciProzorDispecer vozaciProzorDispecer = new VozaciProzorDispecer(taksiSluzba);
+		vozaciProzorDispecer.setVisible(true);
+		
+		VoznjeProzorDispecer voznjeProzorDispecer = new VoznjeProzorDispecer(taksiSluzba);
+		voznjeProzorDispecer.setVisible(true);
 
 }}
