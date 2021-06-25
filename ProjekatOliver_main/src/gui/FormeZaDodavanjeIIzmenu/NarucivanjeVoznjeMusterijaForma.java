1e394da92d26ee1051b3abea37adfe85abe3ec07
Oliver1234-eng <oliver.kojic22@gmail.com>
2021-06-25 21:30:44 +0200

    UOP deo zavrsen

--- ProjekatOliver_main/src/gui/FormeZaDodavanjeIIzmenu/NarucivanjeVoznjeMusterijaForma.java
+++ ProjekatOliver_main/src/gui/FormeZaDodavanjeIIzmenu/NarucivanjeVoznjeMusterijaForma.java
@@ -101,7 +101,7 @@ public class NarucivanjeVoznjeMusterijaForma extends JFrame {
 		String voznjaId = Integer.toString(random);
 		txtIdVoznje.setText(voznjaId);
 		txtIdVoznje.setEditable(false);
-		cbImeMusterije.setEnabled(false);
+//		cbImeMusterije.setEnabled(false);
 		cbImeVozaca.setEnabled(false);
 		double brojPredjenihKilometara = 0;
 		String brojKilometara = Double.toString(brojPredjenihKilometara);
